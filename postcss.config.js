74ed81230817004abca64600910a9e022d45fbbf
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-01-21 12:30:35 +0000

    wrote import comands for build css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require("autoprefixer")],
+  plugins: [
+    require("autoprefixer"),
+    require("postcss-import"),
+    require("postcss-partial-import"),
+  ],
 };
